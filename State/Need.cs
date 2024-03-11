024840af3d495e91f9fb653adfcf64407e970f0e
Jared Blackburn <BlackJar72@yahoo.com>
2024-03-11 11:38:57 -0400

    Improved comments on Need.cs code

--- State/Need.cs
+++ State/Need.cs
@@ -20,25 +20,32 @@ namespace CharacterModel {
         }
 
 
-        // For simple needs that just decay over time; may be used as part of some less complex needs
+
+        /// <summary>
+        /// For simple needs that just decay over time; may be used as part of some less complex needs
+        /// </summary>
         public void Decay() {
             value -= (decayRate * Time.deltaTime) / TIME_SCALE;
             Bound();
         }
 
 
-        // For applying gradual changes caused by external influences, typically from other needs.
-        // This is notably used for the Health need, which decreases when other physiologic needs are low
-        // (or imbalanced?) and increases when those needs are in a good ranges.  Also likely to be used
-        // for needs that recharge slowly during activities like sleeping or eating.
+        /// <summary>
+        /// For applying gradual changes caused by external influences, typically from other needs.
+        /// This is notably used for the Health need, which decreases when other physiologic needs are low
+        /// (or imbalanced?) and increases when those needs are in a good ranges.  Also likely to be used
+        /// for needs that recharge slowly during activities like sleeping or eating.
+        /// </summary>
         public void ApplySituationChange(float amount) {
             value += (amount * Time.deltaTime) / TIME_SCALE;
             Bound();
         }
 
 
-        // For needs that track a current value from elsewhere;
-        // mostly for the Situational need (environment + comfort + entertainment)
+        /// <summary>
+        /// For needs that track a current value from elsewhere;
+        /// mostly for the Situational need (environment + comfort + entertainment)
+        /// </summary>
         public void TrackTargetValue(float target) {
             float amount = ((target - value) * Time.deltaTime) / TIME_SCALE;
             value += (amount * 0.25f) + Mathf.Max(amount, 0);
@@ -46,39 +53,49 @@ namespace CharacterModel {
         }
 
 
-        // Add a discrete amount to the need, as might happen if an action or in-game event changes the need by a
-        // certain amount all at once.  This is intended for both increases and decreases; for decreases, just add
-        // the number.
-        // A likely common use (with a negative value) would be applying the effects of an injury to health
+        /// <summary>
+        /// Add a discrete amount to the need, as might happen if an action or in-game event changes the need by a
+        /// certain amount all at once.  This is intended for both increases and decreases; for decreases, just add
+        /// the number.
+        /// A likely common use (with a negative value) would be applying the effects of an injury to health
+        /// </summary>
         public void Add(float amount) {
             value += amount;
             Bound();
         }
 
 
-        // For rare potential circumstances when a need should be set directly.
-        // This should also be used for loading saves.
+        /// <summary>
+        /// For rare potential circumstances when a need should be set directly.
+        /// This should also be used for loading saves.
+        /// </summary>
         public void Set(float amount) {
             value = Mathf.Clamp(amount,  0.0f, 1.0f);
         }
 
 
-        // Convenience method to instantly fill; probably only used for with cheats.
+        /// <summary>
+        /// Convenience method to instantly fill; probably only used for with cheats.
+        /// </summary>
         public void Fill() {
             value = 1.0f;
         }
 
 
-        // Convenience method to instantly zero-out; With some needs this will lead to instant death.
+        /// <summary>
+        /// Convenience method to instantly zero-out; With some needs this will lead to instant death.
+        /// </summary>
         public void Empty() {
             value = 1.0f;
         }
 
 
-        // Get the strength of the current motivation to fulfill this need
-        // This may need to be tweaked through testing, though the basic concept should work.
-        // Basically the lower the need the higher the drive, with very low need becoming
-        // increasingly intense, especially when entering the danger zone for complete depletion.
+        /// <summary>
+        /// Get the strength of the current motivation to fulfill this need
+        /// This may need to be tweaked through testing, though the basic concept should work.
+        /// Basically the lower the need the higher the drive, with very low need becoming
+        /// increasingly intense, especially when entering the danger zone for complete depletion.
+        /// </summary>
         public float GetDrive() {
             return ((1.2f - value) / Mathf.Clamp(value, 0.01f, 0.5f) * importance);
         }
