27e22458a7ddac52b4af0d97a6f827e2ffefca74
Jared Blackburn <BlackJar72@yahoo.com>
2024-03-11 11:34:14 -0400

    Added prototype code for individual needs

---
+++ State/Need.cs
@@ -0,0 +1,89 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+namespace CharacterModel {
+
+    public class Need {
+        const float TIME_SCALE = 60f; // FIXME: Connect to universal time scale
+        [SerializeField] [HideInInspector] float value;
+        [SerializeField] float decayRate = 0.25f;
+        [SerializeField] float importance = 1.0f;
+
+
+        public float Value => value;
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Bound() {
+            value = Mathf.Clamp(value,0.0f, 1.0f);
+        }
+
+
+        // For simple needs that just decay over time; may be used as part of some less complex needs
+        public void Decay() {
+            value -= (decayRate * Time.deltaTime) / TIME_SCALE;
+            Bound();
+        }
+
+
+        // For applying gradual changes caused by external influences, typically from other needs.
+        // This is notably used for the Health need, which decreases when other physiologic needs are low
+        // (or imbalanced?) and increases when those needs are in a good ranges.  Also likely to be used
+        // for needs that recharge slowly during activities like sleeping or eating.
+        public void ApplySituationChange(float amount) {
+            value += (amount * Time.deltaTime) / TIME_SCALE;
+            Bound();
+        }
+
+
+        // For needs that track a current value from elsewhere;
+        // mostly for the Situational need (environment + comfort + entertainment)
+        public void TrackTargetValue(float target) {
+            float amount = ((target - value) * Time.deltaTime) / TIME_SCALE;
+            value += (amount * 0.25f) + Mathf.Max(amount, 0);
+            Bound();
+        }
+
+
+        // Add a discrete amount to the need, as might happen if an action or in-game event changes the need by a
+        // certain amount all at once.  This is intended for both increases and decreases; for decreases, just add
+        // the number.
+        // A likely common use (with a negative value) would be applying the effects of an injury to health
+        public void Add(float amount) {
+            value += amount;
+            Bound();
+        }
+
+
+        // For rare potential circumstances when a need should be set directly.
+        // This should also be used for loading saves.
+        public void Set(float amount) {
+            value = Mathf.Clamp(amount,  0.0f, 1.0f);
+        }
+
+
+        // Convenience method to instantly fill; probably only used for with cheats.
+        public void Fill() {
+            value = 1.0f;
+        }
+
+
+        // Convenience method to instantly zero-out; With some needs this will lead to instant death.
+        public void Empty() {
+            value = 1.0f;
+        }
+
+
+        // Get the strength of the current motivation to fulfill this need
+        // This may need to be tweaked through testing, though the basic concept should work.
+        // Basically the lower the need the higher the drive, with very low need becoming
+        // increasingly intense, especially when entering the danger zone for complete depletion.
+        public float GetDrive() {
+            return ((1.2f - value) / Mathf.Clamp(value, 0.01f, 0.5f) * importance);
+        }
+
+
+    }
+
+}
